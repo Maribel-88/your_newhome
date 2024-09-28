0432beb1db8b61ff976ed6bd4b297abd36687089
Maribel Pajares <qiangrenlupu@gmail.com>
2024-09-28 18:05:03 +0000

    Added properties views and templates

--- properties/urls.py
+++ properties/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')  
+    path('', views.all_properties, name='all_properties')  
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Property
 
 # Create your views here.
+
+def all_properties(request):
+    """ A view to return all properties, including sorting and searching queries """
+
+    properties = Property.objects.all()
+
+    context = {
+        'properties': properties
+    }
+    return render(request, 'properties/properties.html', context)
