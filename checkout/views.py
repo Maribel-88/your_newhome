542dc0c85bb2554062f5e5baf0e979cc0bb9d32c
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-09 14:15:24 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H7S2yEXZJ64Aa3zH9yjBZ4Iy2ZgZKVOXOLuVAXjdhPHjai1xB870tMh2MtU3AizipzkItfXyD6U9nHQz5FXdyax00cSBMGuEP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
