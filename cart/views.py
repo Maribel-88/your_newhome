31fb6bb2e63a75fa18a376c34c1f6367b712bed0
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-09 01:38:42 +0000

    Added shopping cart preview and additional notification messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from properties.models import Property 
@@ -12,13 +12,14 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified property to the shopping cart """
 
-    property = Property.objects.get(pk=item_id)
+    property = get_object_or_404(Property, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {property.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {property.name} to your cart')
@@ -29,13 +30,16 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """ Adjust the quantity of the specified property to the shopping cart """
 
+    property = get_object_or_404(Property, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {property.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {property.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -44,11 +48,14 @@ def remove_item_cart(request, item_id):
     """Remove the item from the shopping cart """
 
     try:
+        property = get_object_or_404(Property, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Removed {property.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
     
     except Exception as e:
+        messages.error(request,f'Error removing item: {e}')
         return HttpResponse(status=500)
--- properties/views.py
+++ properties/views.py
@@ -40,7 +40,7 @@ def all_properties(request):
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('properties'))
+                return redirect(reverse('all_properties'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             properties = properties.filter(queries)
