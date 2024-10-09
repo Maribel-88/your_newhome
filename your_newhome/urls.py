fb19cc23861a3714128836d3bcc509c8313974ae
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-09 13:25:16 +0000

    added checkout views and templates

--- your_newhome/urls.py
+++ your_newhome/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('properties/', include('properties.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
